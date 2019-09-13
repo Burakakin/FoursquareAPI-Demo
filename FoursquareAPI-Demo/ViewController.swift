c10b9548622d18cb00d54e8dfbbeeb7e4accfce3
Burak Akın <akinburak5@gmail.com>
2019-09-13 16:25:01 +0300

    Initial Commit

---
+++ FoursquareAPI-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareAPI-Demo
+//
+//  Created by Burak Akin on 13.09.2019.
+//  Copyright © 2019 Burak Akin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
