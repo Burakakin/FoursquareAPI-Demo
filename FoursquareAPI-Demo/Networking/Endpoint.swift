c74ef671c2c16866c0e1960fb91b86bf1d1cb6cd
Burak Akın <akinburak5@gmail.com>
2019-09-13 16:49:28 +0300

    Folder structure implemented and genericAPI file added

---
+++ FoursquareAPI-Demo/Networking/Endpoint.swift
@@ -0,0 +1,9 @@
+//
+//  Endpoint.swift
+//  FoursquareAPI-Demo
+//
+//  Created by Burak Akin on 13.09.2019.
+//  Copyright © 2019 Burak Akin. All rights reserved.
+//
+
+import Foundation
---
+++ FoursquareAPI-Demo/Networking/FoursquareClient.swift
@@ -0,0 +1,9 @@
+//
+//  FoursquareClient.swift
+//  FoursquareAPI-Demo
+//
+//  Created by Burak Akin on 13.09.2019.
+//  Copyright © 2019 Burak Akin. All rights reserved.
+//
+
+import Foundation
